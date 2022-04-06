0abc0e6b5ce1d3d505e30f177c751f8252a41405
tunalad <novica.arsic@aol.com>
2022-04-06 17:21:24 +0200

    for ncmpcpp notification, using dunstify instead of notify-send

--- .config/ncmpcpp/scripts/song_notify.sh
+++ .config/ncmpcpp/scripts/song_notify.sh
@@ -2,6 +2,6 @@
 
 readonly MUSIC_DIR="${HOME}/Music"
 readonly SONG_DIR="$(dirname "$(mpc --format '%file%' current)")"
-readonly ALBUM_ART_PATH="${MUSIC_DIR}/${SONG_DIR}/cover.png"
+readonly ALBUM_ART_PATH="${MUSIC_DIR}/${SONG_DIR}/cover.jpg"
 
-notify-send -t 10000 -i "${ALBUM_ART_PATH}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
+dunstify -r 9993 -t 10000 -i "${ALBUM_ART_PATH}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
