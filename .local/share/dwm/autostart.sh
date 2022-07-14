78655f9eb4ac4e2b12a21a03a474a8cef98c7155
tunalad <novica.arsic@aol.com>
2022-07-14 16:06:38 +0200

    dwm autostarting script solution

---
+++ .local/share/dwm/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+xrandr --output LVDS-1 --set "scaling mode" "Full"
+
+if zenity --timeout 8 --question --text="Autostart programs?" --no-wrap --ok-label "No" --cancel-label "Yes"; then
+	exit 0
+else
+	megasync &
+	cadence --minimized &
+	~/.local/bin/./Buttercup-linux-x64.AppImage --no-window &
+	ferdi &
+fi
