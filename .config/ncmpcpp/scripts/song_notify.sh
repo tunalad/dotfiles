71dda7d8dc5b92a6a5104de1ce01a065d123f6af
tunalad <novica.arsic@aol.com>
2022-08-07 19:50:00 +0200

    ncmpcpp: song notify background is album cover dependant

--- .config/ncmpcpp/scripts/song_notify.sh
+++ .config/ncmpcpp/scripts/song_notify.sh
@@ -4,4 +4,10 @@ readonly MUSIC_DIR="${HOME}/Music"
 readonly SONG_DIR="$(dirname "$(mpc --format '%file%' current)")"
 readonly ALBUM_ART_PATH="${MUSIC_DIR}/${SONG_DIR}/cover.jpg"
 
-dunstify -r 9993 -t 10000 -i "${ALBUM_ART_PATH}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
+DOMINANT_COLORS=$(convert "${ALBUM_ART_PATH}" -scale 50x50! -depth 8 +dither -colors 8 -format "%c" histogram:info: |\
+    sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p')
+BACKGROUND_COLOR=($DOMINANT_COLORS)
+
+
+dunstify -h string:bgcolor:#"${BACKGROUND_COLOR: -6}" -r 9993 -t 10000 -i "${ALBUM_ART_PATH}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
+#dunstify -r 9993 -t 10000 -i "${ALBUM_ART_PATH}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
