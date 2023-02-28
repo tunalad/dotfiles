985fe233ea6e2ae518847addd965538b7d4522a5
tunalad <novica.arsic@aol.com>
2023-02-28 23:00:25 +0100

    ncmpcpp: visual changes
    disabled visualizer, added lyrics sources, notifications change
    background color depending on the album artwork

--- .config/ncmpcpp/scripts/song_notify.sh
+++ .config/ncmpcpp/scripts/song_notify.sh
@@ -2,12 +2,14 @@
 
 readonly MUSIC_DIR="${HOME}/Music"
 readonly SONG_DIR="$(dirname "$(mpc --format '%file%' current)")"
-readonly ALBUM_ART_PATH="${MUSIC_DIR}/${SONG_DIR}/cover.jpg"
+ALBUM_ART_PATH="${MUSIC_DIR}/${SONG_DIR}/cover.jpg"
 
-DOMINANT_COLORS=$(convert "${ALBUM_ART_PATH}" -scale 50x50! -depth 8 +dither -colors 8 -format "%c" histogram:info: |\
+[[ ! -f $ALBUM_ART_PATH ]] && ALBUM_ART_PATH="$HOME/Music/fallback.png"
+
+DOMINANT_COLORS=$(convert "${ALBUM_ART_PATH}" \
+    -scale 50x50! -depth 8 +dither -colors 8 -format "%c" histogram:info: |\
     sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p')
 BACKGROUND_COLOR=($DOMINANT_COLORS)
-
 
 dunstify -h string:bgcolor:#"${BACKGROUND_COLOR: -6}" -r 9993 -t 10000 -i "${ALBUM_ART_PATH}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
 #dunstify -r 9993 -t 10000 -i "${ALBUM_ART_PATH}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
