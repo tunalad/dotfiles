2d4ab83899773053dab60fd267f86177990e6bdd
tunalad <28594843+tunalad@users.noreply.github.com>
2024-02-29 21:16:13 +0100

    ncmpcpp: fixed script background color picking
    also minor ncmpcpp stuff, like now I'm showing the album artist too

--- .config/ncmpcpp/scripts/song_notify.sh
+++ .config/ncmpcpp/scripts/song_notify.sh
@@ -2,14 +2,17 @@
 
 readonly MUSIC_DIR="${HOME}/Music"
 readonly SONG_DIR="$(dirname "$(mpc --format '%file%' current)")"
-ALBUM_ART_PATH="${MUSIC_DIR}/${SONG_DIR}/cover.jpg"
 
-[[ ! -f $ALBUM_ART_PATH ]] && ALBUM_ART_PATH="$HOME/Music/fallback.png"
+ALBUM_ART_PATH=$(find "${MUSIC_DIR}/${SONG_DIR}" -type f -iname 'cover.*' -print -quit)
+echo "$ALBUM_ART_PATH"
+
+[[ ! -f $ALBUM_ART_PATH ]] && ALBUM_ART_PATH="$MUSIC_DIR/fallback.png"
 
 DOMINANT_COLORS=$(convert "${ALBUM_ART_PATH}" \
     -scale 50x50! -depth 8 +dither -colors 8 -format "%c" histogram:info: |\
     sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p')
-BACKGROUND_COLOR=($DOMINANT_COLORS)
 
-dunstify -h string:bgcolor:#"${BACKGROUND_COLOR: -6}" -r 9993 -t 10000 -i "${ALBUM_ART_PATH}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
-#dunstify -r 9993 -t 10000 -i "${ALBUM_ART_PATH}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
+# Assuming the first dominant color is the most relevant for the background
+BACKGROUND_COLOR=$(echo "$DOMINANT_COLORS" | head -n 1 | cut -d',' -f2)
+
+dunstify -h string:bgcolor:"$BACKGROUND_COLOR" -r 9993 -t 10000 -i "$ALBUM_ART_PATH" "Now Playing..." "$(mpc --format '%title%\n%artist%\n%album%' current)"
