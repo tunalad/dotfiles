132f801447fa13ecec4ffaf1396c6be1e7a911db
tunalad <28594843+tunalad@users.noreply.github.com>
2024-08-17 23:09:42 +0200

    nvim: added godot connection support

--- .config/nvim/lua/tunalad/init.lua
+++ .config/nvim/lua/tunalad/init.lua
@@ -8,4 +8,15 @@ vim.g.barbar_auto_setup = false
 
 -- colorscheme
 vim.g.codedark_transparent = 1
+--vim.g.codedark_italics = 1
+vim.cmd("highlight Pmenu guibg=NONE")
 vim.cmd.colorscheme("codedark")
+
+-- godot project
+local gdproject = io.open(vim.fn.getcwd() .. "/project.godot", "r")
+if gdproject then
+    require("lspconfig").gdscript.setup(vim.lsp.protocol.make_client_capabilities())
+
+    io.close(gdproject)
+    vim.fn.serverstart("127.0.0.1:55432")
+end
