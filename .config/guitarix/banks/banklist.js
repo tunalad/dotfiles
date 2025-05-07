bc0622b0948aadae870f82393c673b4b6ba5296c
tunalad <28594843+tunalad@users.noreply.github.com>
2025-05-07 22:26:09 +0200

    guitarix: just adding all the banks

---
+++ .config/guitarix/banks/banklist.js
@@ -0,0 +1,10 @@
+[
+  ["tvna", "tvna.gx", 1, 0, [1, 2], 1732383399],
+  ["Art", "Art.gx", 1, 0, [1, 2], 1716397327],
+  ["nuclear", "nuclear.gx", 1, 0, [1, 2], 1703524498],
+  ["tvna blackbox", "tvna blackbox.gx", 1, 0, [1, 2], 1743286752],
+  ["stratojaune", "stratojaune.gx", 1, 0, [1, 2], 1732383233],
+  ["Chaos", "Chaos.gx", 1, 0, [1, 2], 1666036060],
+  ["tvna (-2024)", "tvna %28-2024%29.gx", 1, 0, [1, 2], 1705681598],
+  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1732384427]
+]
