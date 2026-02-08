9d7723d57fc36e52d25fc0325496eec455be3a9b
tunalad <28594843+tunalad@users.noreply.github.com>
2026-02-08 20:23:54 +0100

    session.sh checks if systemd or pass is required

--- .local/scripts/session.sh
+++ .local/scripts/session.sh
@@ -1,12 +1,47 @@
 #!/bin/sh
 
-#pick=$(printf "Power off\nReboot\nLock\nLogout" | rofi -dmenu -i)
-pick=$(echo " Power off\n Reboot\n Lock\n" | dmenu -i -l 5)
-#pick=$(echo -e " Lock\n Power off\n Reboot" | dmenu -i -l 5)
+INIT=$(basename $(cat /proc/1/comm))
+
+auth_required() {
+    notify-send "Authentication Required" "Power management requires password. Please check polkit rules."
+    pass=$(zenity --password --title="Authentication Required")
+    echo "$pass"
+}
+
+call_shutdown() {
+    if [ "$INIT" = "systemd" ]; then
+        systemctl poweroff || {
+            pass=$(auth_required)
+            echo "$pass" | sudo -S systemctl poweroff
+        }
+    else
+        loginctl poweroff || {
+            pass=$(auth_required)
+            echo "$pass" | sudo -S loginctl poweroff
+        }
+    fi
+}
+
+call_reboot() {
+    if [ "$INIT" = "systemd" ]; then
+        systemctl reboot || {
+            pass=$(auth_required)
+            echo "$pass" | sudo -S systemctl reboot
+        }
+    else
+        loginctl reboot || {
+            pass=$(auth_required)
+            echo "$pass" | sudo -S loginctl reboot
+        }
+    fi
+}
+
+#pick=$(echo " Power off\n Reboot\n Lock\n Logout" | dmenu -i -l 5)
+pick=$(echo " Power off\n Reboot\n Lock" | dmenu -i -l 5)
 
 case $pick in
-" Power off") shutdown now ;;
-" Reboot") reboot ;;
+" Power off") call_shutdown ;;
+" Reboot") call_reboot ;;
 " Lock") slock ;;
 " Logout") killall dwm ;;
 *) exit 1 ;;
